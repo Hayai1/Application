a0a585bb3a6545f909914aad3344fc816d10cf04
Kenshi <60287993+Hayai1@users.noreply.github.com>
2022-11-22 14:43:38 +0000

    code/file CleanUp

---
+++ scripts/game.py
@@ -0,0 +1,29 @@
+
+import pygame
+from scripts.world import World
+from scripts.player import Player
+from scripts.window import Window
+from scripts.camera import Camera
+from scripts.input import Input
+BLACK = (0, 0, 0)
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = Window((700,500),"Nea Project",60)
+        self.surface = self.window.createNewSurface((300,200))
+        self.world = World(50)
+        self.player = Player(0,0)
+        self.camera = Camera(self.player)
+        self.input = Input()
+    def runGame(self):
+        # -------- Main Program Loop -----------
+        while True:
+            self.window.screen.fill((0,0,0))
+            self.input.update(self.player)
+            self.camera.update()
+            self.surface.fill(BLACK)
+            self.world.update(self.surface, self.camera)
+            self.player.update(self.world,self.surface,self.camera)
+            self.window.update(self.surface)
+    
+
--- scripts/player.py
+++ scripts/player.py
@@ -1,5 +1,5 @@
 import pygame
-from character import Character
+from scripts.character import Character
 
 class Player(Character):
     
